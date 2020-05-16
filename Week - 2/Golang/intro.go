f91508847fdf80e28cf4f769daa2c5f0f73ed1ab
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2020-05-17 00:57:57 +0530

    datatypes and plan

--- Week - 2/Golang/intro.go
+++ Week - 2/Golang/intro.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("What's compilin ayy coders???")
+
+	var name string = "Dheeraj"
+	fmt.Println(name)
+
+	var description string
+	description = "is awesome!!!"
+
+	fmt.Println(name + " " + description)
+
+	var age uint16
+	age = 18
+
+	fmt.Println(name, "is", age, "years experienced!")
+
+	var theBest bool = false
+	var oneOfTheBest bool = true
+	fmt.Println("Is", name, "the best coders out there? ", theBest, ". But is he one of the best? ", oneOfTheBest)
+
 }
